5fc84c73da7b71e56ef0a9da68ccfb115efdce05
CaptMoar <Sebastian.DiazG@outlook.com>
2020-01-07 01:14:07 -0300

    Se agrega Componente de carga Bar

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Loader from './loader/Loader';
+
+import '../css/App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div class="app">
+          
+          <Loader>
+
+          </Loader>
+          
+      </div>
+    )
+  }
+}
